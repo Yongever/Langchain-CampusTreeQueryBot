6168dc4aeacf156758a6dbab3da1a0313c03f101
Yongever <123290814+Yongever@users.noreply.github.com>
2024-11-16 14:13:58 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import openai 
+import os 
 
 st.set_page_config(page_title="ChatGPT")
 st.title("Questions with our campus trees dataset?")
@@ -8,6 +10,54 @@ import numpy as np
 with st.chat_message("user"):
     st.write("Hello 👋")
     # st.line_chart(np.random.randn(30, 3))
+
+# Ask user for their OpenAI API key via `st.text_input`.
+# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
+# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
+openai_api_key = st.text_input("OpenAI API Key", type="password")
+# openai_api_key = os.getenv('OPENAI_API_KEY')
+
+if not openai_api_key:
+    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
+else:
+
+    # Create an OpenAI client.
+    client = openai(api_key=openai_api_key)
+
+    # Create a session state variable to store the chat messages. This ensures that the
+    # messages persist across reruns.
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    # Display the existing chat messages via `st.chat_message`.
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    # Create a chat input field to allow the user to enter a message. This will display
+    # automatically at the bottom of the page.
+    if prompt := st.chat_input("What is up?"):
+
+        # Store and display the current prompt.
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        # Generate a response using the OpenAI API.
+        stream = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+
+        # Stream the response to the chat using `st.write_stream`, then store it in 
+        # session state.
+        with st.chat_message("assistant"):
+            response = st.write_stream(stream)
+        st.session_state.messages.append({"role": "assistant", "content": response})
 
 
 uploaded_file = st.file_uploader("Upload a csv file", type=["csv"])
