2420b3cc55d25d3a4573a7ad46d4cfe08c1a3ee1
Yongever <123290814+Yongever@users.noreply.github.com>
2024-11-16 17:23:13 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -11,7 +11,7 @@ from langchain_community.llms import OpenAI
 
 # openai.api_key = st.secrets["OPENAI_API_KEY"]
 
-def get_answer_csv(file: TextIO, query: str) -> str:
+def get_answer_csv(file: TextIO, API_KEY, query: str) -> str:
     """
     Returns the answer to the given query by querying a CSV file.
 
@@ -23,14 +23,17 @@ def get_answer_csv(file: TextIO, query: str) -> str:
     - answer (str): the answer to the query from the CSV file.
     """
     # Load the CSV file as a Pandas dataframe
-    # df = pd.read_csv(file)
+    #df = pd.read_csv(file)
     #df = pd.read_csv("titanic.csv")
 
+    llm = OpenAI(openai_api_key=API_KEY, temperature=0)
+
     # Create an agent using OpenAI and the Pandas dataframe
-    agent = create_csv_agent( OpenAI(temperature=0), file, verbose=True, allow_dangerous_code=True)
+    agent = create_csv_agent( llm, file, verbose=True, allow_dangerous_code=True)
     #agent = create_pandas_dataframe_agent(OpenAI(temperature=0), df, verbose=False)
 
     # Run the agent on the given query and return the answer
     #query = "whats the square root of the average age?"
     answer = agent.run(query)
+    
     return answer
