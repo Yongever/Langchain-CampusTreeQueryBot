3b7489365469a9c8df768963e97573963772186f
Yongever <123290814+Yongever@users.noreply.github.com>
2024-11-18 10:57:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -14,8 +14,6 @@ st.write(
 st.write(
     "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
 )
-st.write("You can also learn how to build this app step by step by [following streamlit tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
-)
 
 openai_api_key = st.text_input("OpenAI API Key", type="password")
 # OPENAI_API_KEY=openai_api_key
